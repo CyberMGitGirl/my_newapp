8c3dade6c3d0811315aa883bef4b3436c064f787
CyberMGitGirl <cynthiaadsright@outlook.com>
2025-11-30 08:31:27 -0800

    did review js

--- lib/pages/model/review.dart
+++ lib/pages/model/review.dart
@@ -1,9 +1,25 @@
 import 'package:my_newapp/pages/model/user_detail.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'review.g.dart';
+
+@JsonSerializable()
 class Review {
   final String message;
   final UserDetail user;
   final DateTime date;
 
   Review({required this.message, required this.user, required this.date});
+
+  Map<String, dynamic> toJson() => _$ReviewToJson(this);
+
+  factory Review.fromJson(Map<String, dynamic> json) => _$ReviewFromJson(json);
 }
+
+/*class Review {
+  final String message;
+  final UserDetail user;
+  final DateTime date;
+
+  Review({required this.message, required this.user, required this.date});
+} */
